915827d09950b89088dcdf8273295c9b96cfd0e3
tjdcjf1996 <r_louis@naver.com>
2024-10-29 09:41:00 +0900

    update: "중복 로그인 비허용"

--- src/constants/handlerIds.js
+++ src/constants/handlerIds.js
@@ -1,4 +1,5 @@
 export const RESPONSE_SUCCESS_CODE = 0;
+export const RESPONSE_FAIL_CODE = 1;
 
 export const HANDLER_IDS = {
   INITIAL: 0,
--- src/handlers/init.handler.js
+++ src/handlers/init.handler.js
@@ -1,18 +1,26 @@
 import { createResponse } from '../../utils/response/createResponse.js';
-import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from '../constants/handlerIds.js';
-import { addUser } from '../session/user.sessions.js';
+import { HANDLER_IDS, RESPONSE_FAIL_CODE, RESPONSE_SUCCESS_CODE } from '../constants/handlerIds.js';
+import { addUser, getUserById } from '../session/user.sessions.js';
 import { joinGameHandler } from './game.handler.js';
 
 export const initHandler = async (socket, userId, payload) => {
-  const { user, lastX, lastY } = await addUser(socket, userId, payload.latency);
-  joinGameHandler(user);
-
-  const initResponse = createResponse(
-    HANDLER_IDS.INITIAL,
-    RESPONSE_SUCCESS_CODE,
-    { userId, lastX, lastY },
-    userId,
-  );
+  let initResponse;
+  const isLoggedIn = getUserById(userId);
+  // 이미 로그인 된 userId라면, RESPONSE_FAIL_CODE 반환
+  if (isLoggedIn) {
+    initResponse = createResponse(HANDLER_IDS.INITIAL, RESPONSE_FAIL_CODE, { userId }, userId);
+  } else {
+    // 유저 세션 등록
+    const { user, lastX, lastY } = await addUser(socket, userId, payload.latency);
+    // 게임 세션 등록
+    joinGameHandler(user);
+    initResponse = createResponse(
+      HANDLER_IDS.INITIAL,
+      RESPONSE_SUCCESS_CODE,
+      { userId, lastX, lastY },
+      userId,
+    );
+  }
 
   socket.write(initResponse);
 };
