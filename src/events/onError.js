3c998e5fbf74a092694bc13e4df3779a99c1ea35
tjdcjf1996 <r_louis@naver.com>
2024-11-01 02:00:28 +0900

    add: "onError 내용 추가"
    dddd

--- src/events/onError.js
+++ src/events/onError.js
@@ -1,3 +1,8 @@
+import { exitGameHandler } from '../handlers/game.handler.js';
+import { removeUser } from '../session/user.sessions.js';
+
 export const onError = (socket) => (err) => {
+  exitGameHandler(socket);
+  removeUser(socket);
   console.error(err);
 };
