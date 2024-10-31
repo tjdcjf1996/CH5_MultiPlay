b4168a22794ea993a6943157d5aba62cb9e1ec79
tjdcjf1996 <r_louis@naver.com>
2024-10-31 23:48:18 +0900

    update:"추측항법 추가"

--- src/classes/user.class.js
+++ src/classes/user.class.js
@@ -6,12 +6,17 @@ class User {
     this.socket = socket;
     this.x = 0;
     this.y = 0;
+    this.preX = 0;
+    this.preY = 0;
+    this.speed = 3;
     this.latency = latency;
     this.sequence = 0;
     this.lastUpdateTime = Date.now();
   }
 
   updatePosition(x, y) {
+    this.preX = this.x;
+    this.preY = this.y;
     this.x = x;
     this.y = y;
     this.lastUpdateTime = Date.now();
@@ -35,15 +40,21 @@ class User {
   }
 
   calculatePosition(latency) {
-    const timeDiff = latency / 1000;
-    const speed = 1;
-    const distance = speed * timeDiff;
+    if (this.x === this.preX && this.y === this.preY) {
+      return { x: this.x, y: this.y };
+    }
+
+    const timeDiff = (Date.now() - this.lastUpdateTime + latency) / 1000;
+
+    const distance = this.speed * timeDiff; // 거속시
+
+    const directX = this.x !== this.lastX ? Math.sign(this.x - this.lastX) : 0; // 방향
+    const directY = this.y !== this.lastY ? Math.sign(this.y - this.lastY) : 0;
 
     return {
-      x: this.x + distance,
-      y: this.y,
+      x: this.x + directX * distance,
+      y: this.y + directY * distance,
     };
   }
 }
-
 export default User;
